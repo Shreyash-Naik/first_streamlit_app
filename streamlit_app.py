381c9fd73b76cfb67384a4e9b880459c01d2a340
Shreyash-Naik <100992452+Shreyash-Naik@users.noreply.github.com>
2022-07-06 20:25:53 +0530

    Update streamlit_app.py
    Adding Snowflake User Data

--- streamlit_app.py
+++ streamlit_app.py
@@ -38,3 +38,11 @@ streamlit.dataframe(fruityvice_normalized)
 
 #Snowflake Python Connector
 import snowflake.connector
+
+#Adding Snowflake User Data
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cur = my_cnx.cursor()
+my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+my_data_row = my_cur.fetchone()
+streamlit.text("Hello from Snowflake:")
+streamlit.text(my_data_row)
